1bc516b6a85f277d1b5bfc7a5bc3768dbd9477ef
Alexander Metzger <alexander.le@outlook.dk>
2024-12-15 16:42:51 -0800

    use proper secrets mounts for buildtime secrets; rotate secret keys

--- scripts/docker-run-prod.sh
+++ scripts/docker-run-prod.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-docker build --tag 'koel-labs-server-prod' -f ./scripts/Dockerfile.prod $(for i in `cat .env`; do out+="--build-arg $i "; done; echo $out; out="") .
+# Prepare the secret files and build the Docker image
+while IFS='=' read -r key value; do
+    echo -n "$value" > "$key.secret"
+done < .env
+
+docker build --tag 'koel-labs-server-prod' -f ./scripts/Dockerfile.prod $(while IFS='=' read -r key value; do echo "--secret id=$key,src=$key.secret "; done < .env) .
+
+# Clean up the temporary secret files
+while IFS='=' read -r key value; do
+  rm "$key.secret"
+done < .env
 
 open http://localhost:8080 || start chrome \"http://localhost:8080\" || google-chrome 'http://localhost:8080' || echo 'Could not open browser automatically. Please open http://localhost:8080 manually'
 docker run -t -i -p 8080:8080 --env-file .env 'koel-labs-server-prod'
