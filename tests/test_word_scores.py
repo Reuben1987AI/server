1684421ae85b974cb5bde043da68978f2be8d15f
Alexander Metzger <alexander.le@outlook.dk>
2025-08-04 13:32:55 -0700

    formatting

--- tests/test_word_scores.py
+++ tests/test_word_scores.py
@@ -1,28 +1,28 @@
 import json
 import urllib.parse
 
-TARGET = ['kʰ', 'ɔ', 'l', 'ɪ', 'ŋ', 'kʰ', 'ɑ', 'ɹ', 'd', 'd', 'z', 'ɑ', 'ɹ', 'ð', 'ə', 'w', 'eɪ', 'v', 'ə', 'v', 'ð', 'ə', 'f', 'j', 'u', 'tʃ', 'ɜ˞']
+TARGET = ['kʰ', 'ɔ', 'l', 'ɪ', 'ŋ', 'kʰ', 'ɑ', 'ɹ', 'd', 'd', 'z', 'ɑ', 'ɹ', 'ð', 'ə', 'w', 'eɪ', 'v', 'ə', 'v', 'ð', 'ə', 'f', 'j', 'u', 'tʃ', 'ɜ˞']  # fmt: skip
 TARGET_BY_WORDS = [
-          ["calling", ["kʰ", "ɔ", "l", "ɪ", "ŋ"]],
-          ["cards", ["kʰ", "ɑ", "ɹ", "d", "z"]],
-          ["are", ["ɑ", "ɹ"]],
-          ["the", ["ð", "ə"]],
-          ['wave', ["w", "eɪ", "v"]],
-          ["of", ["ə", "v"]],
-          ["the", ["ð", "ə"]],
-          ["future", ["f", "j", "u", "tʃ", "ɜ˞"]],
-      ]
+    ["calling", ["kʰ", "ɔ", "l", "ɪ", "ŋ"]],
+    ["cards", ["kʰ", "ɑ", "ɹ", "d", "z"]],
+    ["are", ["ɑ", "ɹ"]],
+    ["the", ["ð", "ə"]],
+    ["wave", ["w", "eɪ", "v"]],
+    ["of", ["ə", "v"]],
+    ["the", ["ð", "ə"]],
+    ["future", ["f", "j", "u", "tʃ", "ɜ˞"]],
+]
 
 
 def test_score_words_cer(client):
     # Use the same phonemes as target for a perfect match
-    speech = ['kʰ', 'ɔ', 'l', 'ɪ', 'ŋ', 'kʰ', 'ɑ', 'ɹ', 'd', 'd', 'z', 'ɑ', 'ɹ', 'ð', 'ə', 'w', 'eɪ', 'v', 'ə', 'v', 'ð', 'ə', 'f', 'k', 'u', 't', 'ɜ˞']
-    
+    speech = ['kʰ', 'ɔ', 'l', 'ɪ', 'ŋ', 'kʰ', 'ɑ', 'ɹ', 'd', 'd', 'z', 'ɑ', 'ɹ', 'ð', 'ə', 'w', 'eɪ', 'v', 'ə', 'v', 'ð', 'ə', 'f', 'k', 'u', 't', 'ɜ˞']  # fmt: skip
+
     # NOTE: I url encode the json parameters because the speech is a list, this may need fixing
     target_param = urllib.parse.quote(json.dumps(TARGET))
     tbw_param = urllib.parse.quote(json.dumps(TARGET_BY_WORDS))
     speech_param = urllib.parse.quote(json.dumps(speech))
-    
+
     response = client.get(
         f"/score_words_cer?target={target_param}&tbw={tbw_param}&speech={speech_param}"
     )
@@ -36,4 +36,3 @@ def test_score_words_cer(client):
     for eng_word, _, _, score in words[:-1]:
         assert score == 1.0
     words[-1][3] = 0.75
-    
